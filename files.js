221e1657c971234c4a9d76d37f00d2324e268213
FavourtheDev <favourthedeveloper@gmail.com>
2023-08-14 13:06:51 +0100

    Learning Node

--- files.js
+++ files.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
+// Reading a file
+
 // fs.readFile('./docs/blog1.txt', (err, data) => {
 //     if (err) {
 //         console.log(err);
@@ -7,10 +9,33 @@ const fs = require('fs');
 //     console.log(data.toString())
 // })
 
-fs.writeFile('./docs/blog1.txt', "HY, FavourtheDev", () => {
-    console.log("File was written");
-})
+// Writing a file
 
-fs.writeFile('./docs/blog2.txt', "XUP FavourtheDev", () => {
-    console.log("File was written again");
-})
+// fs.writeFile('./docs/blog1.txt', "HY, FavourtheDev", () => {
+//     console.log("File was written");
+// })
+
+// fs.writeFile('./docs/blog2.txt', "XUP FavourtheDev", () => {
+//     console.log("File was written again");
+// })
+
+// Creating directories
+
+if (!fs.existsSync('./assets')) {
+    fs.mkdir('./assets', (err) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log("directory created");
+    })
+} else {
+    fs.rmdir('./assets', (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log("Directory removed sussesfully");
+    })
+}
+
+
+
