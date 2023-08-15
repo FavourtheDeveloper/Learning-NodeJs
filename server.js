970c518950bb4051a884fe6bc6b6d496b5a5f7db
FavourtheDev <favourthedeveloper@gmail.com>
2023-08-16 00:35:41 +0100

    Node Js Learn

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const http = require('http');
-const { log } = require('console');
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
     console.log("Request Made");
     
     res.setHeader('Content-Type', 'text/html')
+
+    let path = './views/';
+    if (req.url == '/') {
+        path += 'index.html';
+    } else if (req.url == '/about') {
+        path += 'about.html';
+    } else {
+        path += '404page.html';
+    }
     
-    res.write("<h1>Hello FavourtheDev</h1>")
-    res.end();
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err)
+            res.end();
+        } else {
+            res.end(data);
+        }
+    })
 });
 server.listen(3000, 'localhost', () => {
     console.log("Server is successful");
