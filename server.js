834e08e8af03da60bdd8ca201a4452c0ae4e108b
FavourtheDev <favourthedeveloper@gmail.com>
2023-08-16 01:48:13 +0100

    Node JS Learn

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 const http = require('http');
 const fs = require('fs')
+const _ = require('lodash')
 
 const server = http.createServer((req, res) => {
-    console.log("Request Made");
+
+    // lodash
+    const num = _.random(0, 20);
+    console.log(num);
     
     res.setHeader('Content-Type', 'text/html')
 
     let path = './views/';
     if (req.url == '/') {
         path += 'index.html';
+        res.statusCode = 200;
     } else if (req.url == '/about') {
         path += 'about.html';
+        res.statusCode = 200;
+    } else if (req.url == '/aboutme') {
+        res.statusCode = 301;
+        res.setHeader('Location', '/about')
+        res.end()
     } else {
         path += '404page.html';
+        res.statusCode = 404;
     }
     
     fs.readFile(path, (err, data) => {
